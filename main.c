f598bc82ac73eb7e1b529d62153f8ef7cc5cf77f
NastvanyAgent <cafourek.david@seznam.cz>
2026-07-11 13:22:26 +0200

    main code implementation

--- main.c
+++ main.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int GetSeed(int val);
+int GetValue(int seed);
 
 int main() {
-	printf("Hello World!\n");
+
+	int seed = 0;
+	int val = 0;
+
+	printf("Input a number to use as a seed: ");	
+	scanf("%d", &seed);
+	printf("The seed %d returns pseudo-random number %d\n", seed, GetValue(seed));
+
+	printf("\n");
+
+	printf("Enter a new value to find its seed: ");
+	scanf("%d", &val);
+	printf("The seed for value %d is %d\n", val, GetSeed(val));
 
 	return 0;
+}
+
+int GetValue(int seed) {
+
+	srand(seed);
+
+	return rand();
+}
+
+int GetSeed(int val) {
+
+	int seed = 0;
+
+	srand(seed);
+
+	while (rand() != val)
+	{
+		seed++;
+		srand(seed);
+	}
+
+	return seed;
 }
